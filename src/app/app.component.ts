5c43c81d080874cec6fc9d55ad7d0da0af17b4cc
elena <elenalistratova11@gmail.com>
2017-08-30 15:31:30 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import { D3Service, D3, Selection } from 'd3-ng2-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private d3: D3;
+  private parentNativeElement: any;
+
   title = 'app';
+
+  constructor(element: ElementRef, d3Service: D3Service) {
+    this.d3 = d3Service.getD3();
+    this.parentNativeElement = element.nativeElement;
+  }
+
+  ngOnInit() {
+    let d3 = this.d3;
+
+    // console.log(d3.selectAll('li'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { D3Service } from 'd3-ng2-service';
+import { BudgetComponent } from './budget/budget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [D3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
